a18a16ce328985ed97096be32b6b61e27b2b632f
KentMT28 <featurete@gmail.com>
2023-11-13 14:29:58 +0700

    menu promo

--- lib/pages/menu_pages.dart
+++ lib/pages/menu_pages.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restaurant_and_order/components/button.dart';
 
 class MenuPage extends StatefulWidget {
-  const MenuPage({super.key});
+  const MenuPage({Key? key}) : super(key: key);
 
   @override
   State<MenuPage> createState() => _MenuPageState();
@@ -10,6 +12,59 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[900],
+        ),
+        title: Text(
+          'WONDERFUL WESTERN EATING',
+          style: TextStyle(color: Colors.grey[900]),
+        ),
+        //appbar
+      ),
+      body: Column(
+        children: [
+          //promobanner
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              children: [
+                // Get 32% Promo text
+                Expanded(
+                  child: Text(
+                    'Get 32% Promo',
+                    style: GoogleFonts.dmSerifDisplay(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                // Redeem button
+                MyButton(
+                  text: "Redeem",
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          //image
+          Image.asset(
+            'images/beef.png',
+            height: 100,
+          ),
+        ],
+      ),
+    );
   }
 }
